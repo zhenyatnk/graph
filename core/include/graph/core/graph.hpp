0c4788a4515cc200bddf1dd1d8dbf0a594cefb76
Zhenya_tnk <tinkalyk@mail.ru>
2019-11-19 23:39:35 +0300

    fx

--- core/include/graph/core/graph.hpp
+++ core/include/graph/core/graph.hpp
@@ -4,6 +4,8 @@
 #include <graph/core/export.hpp>
 
 #include <list>
+#include <array>
+#include <memory>
 
 namespace graph {
 namespace core {
@@ -14,28 +16,41 @@ public:
     struct Element
     {
         using weight_t = uint32_t;
-        
-        Element(const Element&) = delete;
-        Element& operator = (const Element&) = delete;
-        
-        Element(Element&&) = default;
-        
-        std::list<std::pair<weight_t, Element>> outgoing;
+       
+        std::list<std::pair<weight_t, std::shared_ptr<Element>>> outgoing;
     };
+    
+    template<std::size_t count> using matrix_t = std::array<std::array<Element::weight_t,count>,count>;
+    
 public:
-    explicit Graph(Element&& begin)
+    
+    template<std::size_t count>
+    explicit Graph(const matrix_t<count>& matrix)
+    {
+        std::array<std::shared_ptr<Element>, count> elements;
+        
+        for(auto index = 0; index < count; ++index)
+            elements[index] = std::make_shared<Element>();
+        for(auto iElement = 0; iElement < count; ++iElement)
+            for(auto index = 0; index < count; ++index)
+                if(!!matrix[iElement][index])
+                    elements[iElement]->outgoing.emplace_back(std::make_pair(matrix[iElement][index],elements[index]));
+        if(!!count)
+            m_begin = elements[0];
+    }
+    
+    explicit Graph(std::shared_ptr<Element> begin)
     :m_begin(std::move(begin))
     {}
     
-    Element& GetBegin()
+    std::shared_ptr<Element> GetBegin()
     {
         return m_begin;
     }
     
 private:
-    Element m_begin;
+    std::shared_ptr<Element> m_begin;
 };
-
 
 }
 }
--- core/test/graph_test.cpp
+++ core/test/graph_test.cpp
@@ -15,22 +15,45 @@ public:
 
 TEST_F(graph_test, empty)
 {
-    Graph e(Graph::Element{});
+    Graph e(std::make_shared<Graph::Element>());
 }
 
 TEST_F(graph_test, notempty)
 {
-    Graph::Element element{};
-    element.outgoing.emplace_back(std::make_pair(1, Graph::Element{}));
-    Graph e(std::move(element));
+    auto element = std::make_shared<Graph::Element>();
+    element->outgoing.emplace_back(std::make_pair(1, std::make_shared<Graph::Element>()));
+    Graph e(element);
 }
 
 TEST_F(graph_test, GetBegin)
 {
-    Graph::Element element{};
-    element.outgoing.emplace_back(std::make_pair(1, Graph::Element{}));
-    Graph e(std::move(element));
+    auto element = std::make_shared<Graph::Element>();
+    element->outgoing.emplace_back(std::make_pair(1, std::make_shared<Graph::Element>()));
+    Graph e(element);
     
-    ASSERT_EQ(1, e.GetBegin().outgoing.size());
-    ASSERT_EQ(1, e.GetBegin().outgoing.front().first);
+    ASSERT_EQ(1, e.GetBegin()->outgoing.size());
+    ASSERT_EQ(1, e.GetBegin()->outgoing.front().first);
+}
+
+TEST_F(graph_test, matrix)
+{
+    Graph::matrix_t<2> matrix{{{0,5},
+                               {0,0}}};
+    Graph e(matrix);
+    
+    ASSERT_EQ(1, e.GetBegin()->outgoing.size());
+    ASSERT_EQ(5, e.GetBegin()->outgoing.front().first);
+    ASSERT_EQ(0, e.GetBegin()->outgoing.front().second->outgoing.size());
+}
+
+TEST_F(graph_test, matrix_cycle)
+{
+    Graph::matrix_t<2> matrix{{{0,5},
+                               {5,0}}};
+    Graph e(matrix);
+    
+    ASSERT_EQ(1, e.GetBegin()->outgoing.size());
+    ASSERT_EQ(5, e.GetBegin()->outgoing.front().first);
+    ASSERT_EQ(1, e.GetBegin()->outgoing.front().second->outgoing.size());
+    ASSERT_EQ(5, e.GetBegin()->outgoing.front().second->outgoing.front().first);
 }
