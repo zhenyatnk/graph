9905568faf945333dfd1a7e99e23469787a5b4d1
Zhenya_tnk <tinkalyk@mail.ru>
2019-11-20 19:45:45 +0300

    add foreach

--- core/include/graph/core/graph.hpp
+++ core/include/graph/core/graph.hpp
@@ -3,43 +3,55 @@
 #include <graph/core/BaseExceptions.hpp>
 #include <graph/core/export.hpp>
 
-#include <list>
+#include <string>
+#include <vector>
 #include <array>
 #include <memory>
 
 namespace graph {
 namespace core {
-  
-class Graph
+
+using weight_t = uint32_t;
+using ident_t = uint32_t;
+template<std::size_t count> using matrix_t = std::array<std::array<weight_t,count>,count>;
+
+template<typename TData>
+class GraphEx
 {
 public:
     struct Element
     {
-        using weight_t = uint32_t;
-       
-        std::list<std::pair<weight_t, std::shared_ptr<Element>>> outgoing;
+        std::vector<std::pair<weight_t, std::weak_ptr<Element>>> inner;
+        std::vector<std::pair<weight_t, std::shared_ptr<Element>>> outgoing;
+        TData data;
+        ident_t ident;
     };
     
-    template<std::size_t count> using matrix_t = std::array<std::array<Element::weight_t,count>,count>;
-    
 public:
-    
     template<std::size_t count>
-    explicit Graph(const matrix_t<count>& matrix)
+    explicit GraphEx(const matrix_t<count>& matrix)
     {
         std::array<std::shared_ptr<Element>, count> elements;
         
         for(auto index = 0; index < count; ++index)
-            elements[index] = std::make_shared<Element>();
+        {
+            auto element = std::make_shared<Element>();
+            element->ident = index;
+            elements[index] = element;
+        }
+
         for(auto iElement = 0; iElement < count; ++iElement)
             for(auto index = 0; index < count; ++index)
                 if(!!matrix[iElement][index])
+                {
+                    elements[index]->inner.emplace_back(std::make_pair(matrix[iElement][index],std::weak_ptr<Element>(elements[iElement])));
                     elements[iElement]->outgoing.emplace_back(std::make_pair(matrix[iElement][index],elements[index]));
+                }
         if(!!count)
             m_begin = elements[0];
     }
     
-    explicit Graph(std::shared_ptr<Element> begin)
+    explicit GraphEx(std::shared_ptr<Element> begin)
     :m_begin(std::move(begin))
     {}
     
@@ -47,10 +59,52 @@ public:
     {
         return m_begin;
     }
+    const Element& GetBegin()const
+    {
+        return *m_begin;
+    }
     
 private:
     std::shared_ptr<Element> m_begin;
 };
+
+using Graph = GraphEx<void*>;
+
+template<typename Type, typename Function>
+void for_each(const Type& begin, const Function& caller)
+{
+    auto isParent = begin.inner.size() == 0;
+    if(!isParent)
+       caller(begin);
+    for(const auto& element: begin.outgoing)
+    {
+        if(isParent)
+           caller(begin);
+        for_each(*element.second, caller);
+    }
+
+}
+
+template<typename TData>
+std::string ToIdentsString(const GraphEx<TData>& graph)
+{
+    std::string text;
+    for_each(graph.GetBegin(), [&text](const typename GraphEx<TData>::Element& element){ text += std::to_string(element.ident);});
+    return text;
+}
+
+template<std::size_t count>
+std::vector<ident_t> GetMinPath(const matrix_t<count>& matrix)
+{
+    struct Data
+    {
+        std::shared_ptr<typename GraphEx<Data>::Element> previuos;
+        weight_t minWidth;
+    };
+    
+    GraphEx<Data> graph(matrix);
+    return {};
+}
 
 }
 }
--- core/test/graph_test.cpp
+++ core/test/graph_test.cpp
@@ -37,8 +37,10 @@ TEST_F(graph_test, GetBegin)
 
 TEST_F(graph_test, matrix)
 {
-    Graph::matrix_t<2> matrix{{{0,5},
-                               {0,0}}};
+    matrix_t<2> matrix{{
+        {0,5},
+        {0,0}
+    }};
     Graph e(matrix);
     
     ASSERT_EQ(1, e.GetBegin()->outgoing.size());
@@ -48,8 +50,10 @@ TEST_F(graph_test, matrix)
 
 TEST_F(graph_test, matrix_cycle)
 {
-    Graph::matrix_t<2> matrix{{{0,5},
-                               {5,0}}};
+    matrix_t<2> matrix{{
+        {0,5},
+        {5,0}
+    }};
     Graph e(matrix);
     
     ASSERT_EQ(1, e.GetBegin()->outgoing.size());
@@ -57,3 +61,86 @@ TEST_F(graph_test, matrix_cycle)
     ASSERT_EQ(1, e.GetBegin()->outgoing.front().second->outgoing.size());
     ASSERT_EQ(5, e.GetBegin()->outgoing.front().second->outgoing.front().first);
 }
+
+TEST_F(graph_test, matrix_real)
+{
+    matrix_t<3> matrix {{
+        {0,2,3},
+        {0,0,4},
+        {0,0,0}
+    }};
+    Graph e(matrix);
+    
+    ASSERT_EQ(0, e.GetBegin()->ident);
+    ASSERT_EQ(0, e.GetBegin()->inner.size());
+    ASSERT_EQ(2, e.GetBegin()->outgoing.size());
+    
+    ASSERT_EQ(1, e.GetBegin()->outgoing[0].second->ident);
+    ASSERT_EQ(1, e.GetBegin()->outgoing[0].second->inner.size());
+    ASSERT_EQ(1, e.GetBegin()->outgoing[0].second->outgoing.size());
+
+    ASSERT_EQ(2, e.GetBegin()->outgoing[1].second->ident);
+    ASSERT_EQ(2, e.GetBegin()->outgoing[1].second->inner.size());
+    ASSERT_EQ(0, e.GetBegin()->outgoing[1].second->outgoing.size());
+}
+/*
+TEST_F(graph_test, to_string_1)
+{
+    matrix_t<3> matrix {{
+        {0,2,3},
+        {0,0,4},
+        {0,0,0}
+    }};
+    Graph e(matrix);
+    
+    auto text = ToIdentsString(e);
+
+    ASSERT_STREQ("", text.c_str());
+}
+
+TEST_F(graph_test, to_string_2)
+{
+    matrix_t<4> matrix {{
+        {0,1,3,0},
+        {0,0,1,5},
+        {0,0,0,3},
+        {0,0,0,0}
+    }};
+    Graph e(matrix);
+    
+    auto text = ToIdentsString(e);
+
+    ASSERT_STREQ("", text.c_str());
+}*/
+
+class dijkstra_test
+    :public ::testing::Test
+{
+public:
+    void SetUp()
+    {}
+};
+
+TEST_F(dijkstra_test, empty)
+{
+    auto path = GetMinPath(matrix_t<0>{});
+    ASSERT_EQ(0, path.size());
+}
+/*
+TEST_F(dijkstra_test, one)
+{
+    auto path = GetMinPath(matrix_t<1>{{{1}}});
+    ASSERT_EQ(1, path.size());
+}
+
+TEST_F(dijkstra_test, simple)
+{
+    matrix_t<4> matrix {{
+        {0,1,3,0},
+        {0,0,1,5},
+        {0,0,0,3},
+        {0,0,0,0}
+    }};
+    auto path = GetMinPath(matrix);
+    ASSERT_EQ(4, path.size());
+}*/ 
